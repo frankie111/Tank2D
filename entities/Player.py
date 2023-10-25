131ea3e0b7c67dbf68092ca781687a2c39759757
mircozenBalozen <mircea.rautoiu@gmail.com>
2023-10-26 00:07:11 +0300

    Rotation still buggy

--- entities/Player.py
+++ entities/Player.py
@@ -1,5 +1,3 @@
-import math
-
 import pygame.image
 
 from entities.Direction import Direction
@@ -7,53 +5,41 @@ from entities.Direction import Direction
 
 class Player:
     def __init__(self, start_x, start_y, sprite_path="../resources/tank_sprite.png", scale=(100, 100)):
-        self.x = start_x
-        self.y = start_y
         self.velocity = 2
         self.rotation_angle = 0
         # load the original sprite
-        self.original_sprite = pygame.image.load(sprite_path)
+        self.original_sprite = pygame.image.load(sprite_path).convert_alpha()
         # Scale the sprite
         if scale:
             self.original_sprite = pygame.transform.scale(self.original_sprite, scale)
 
         self.sprite = self.original_sprite.copy()  # Ensure that we start with a scaled sprite
         self.sprite_rect = self.sprite.get_rect()
-        self.sprite_rect.center = (self.x, self.y)
+        self.sprite_rect.center = (start_x, start_y)
 
     def draw(self, screen):
-        screen.blit(self.sprite, self.sprite_rect.topleft)  # Draw the tank sprite in the pos of sprite_rect
+        screen.blit(self.sprite, self.sprite_rect)  # Draw the tank sprite in the pos of sprite_rect
 
     def draw_hitbox(self, screen):
-        
+        hitbox_color = (82, 5, 0)
+        hitbox_rect = pygame.Rect(self.sprite_rect.left, self.sprite_rect.top, self.sprite_rect.width,
+                                  self.sprite_rect.height)
+        pygame.draw.rect(screen, hitbox_color, hitbox_rect, 2)
+
     def move(self, direction):
         if direction == Direction.RIGHT:
-            self.x += self.velocity
+            self.sprite_rect.x += self.velocity
 
         if direction == Direction.LEFT:
-            self.x -= self.velocity
+            self.sprite_rect.x -= self.velocity
 
         if direction == Direction.UP:
-            self.y -= self.velocity
+            self.sprite_rect.y -= self.velocity
 
         if direction == Direction.DOWN:
-            self.y += self.velocity
-
-        self.sprite_rect.center = (self.x, self.y)
+            self.sprite_rect.y += self.velocity
 
     def rotate(self, angle):
         self.rotation_angle = angle
-        self.sprite = pygame.transform.rotate(self.original_sprite, math.degrees(angle))
+        self.sprite = pygame.transform.rotate(self.original_sprite, angle)
         self.sprite_rect = self.sprite.get_rect(center=self.sprite_rect.center)
-
-    def get_left(self):
-        return self.x - self.sprite_rect.width / 2
-
-    def get_right(self):
-        return self.x + self.sprite_rect.width / 2
-
-    def get_top(self):
-        return self.y - self.sprite_rect.height / 2
-
-    def get_bottom(self):
-        return self.y + self.sprite_rect.height / 2
--- logic/Game.py
+++ logic/Game.py
@@ -8,17 +8,18 @@ from logic.Canvas import Canvas
 
 
 def get_angle(tank_position, mouse_position):
-    x_diff = mouse_position[0] - tank_position[0]
-    y_diff = mouse_position[1] - tank_position[1]
-    return math.atan2(y_diff, x_diff)
+    x_dist = mouse_position[0] - tank_position[0]
+    y_dist = mouse_position[1] - tank_position[1]
+    angle = math.atan2(y_dist, x_dist)
+    return -math.degrees(angle)
 
 
 class Game:
     def __init__(self, w, h):
         self.width = w
         self.height = h
-        self.player = Player(50, 100)
         self.canvas = Canvas(self.width, self.height, "Tank2D")
+        self.player = Player(50, 100, self.canvas.get_canvas())
 
     def run(self):
         clock = pygame.time.Clock()
@@ -37,26 +38,27 @@ class Game:
             keys = pygame.key.get_pressed()
 
             if (keys[pygame.K_d]
-                    and self.player.get_right() <= self.width - self.player.velocity):
+                    and self.player.sprite_rect.right <= self.width - self.player.velocity):
                 self.player.move(Direction.RIGHT)
 
             if (keys[pygame.K_a]
-                    and self.player.get_left() >= self.player.velocity):
+                    and self.player.sprite_rect.left >= self.player.velocity):
                 self.player.move(Direction.LEFT)
 
             if (keys[pygame.K_w]
-                    and self.player.get_top() >= self.player.velocity):
+                    and self.player.sprite_rect.top >= self.player.velocity):
                 self.player.move(Direction.UP)
 
             if (keys[pygame.K_s]
-                    and self.player.get_bottom() <= self.height - self.player.velocity):
+                    and self.player.sprite_rect.bottom <= self.height - self.player.velocity):
                 self.player.move(Direction.DOWN)
 
             mouse_position = pygame.mouse.get_pos()
             angle = get_angle(self.player.sprite_rect, mouse_position)
-            self.player.rotate(-angle)
+            self.player.rotate(angle)
 
             self.canvas.draw_background()
+            self.player.draw_hitbox(self.canvas.get_canvas())
             self.player.draw(self.canvas.get_canvas())
             self.canvas.update()
 
